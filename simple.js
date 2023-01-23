97c531a307f4153269311f548f3a9fb2bd4dabcb
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-01-23 09:25:38 -0500

    simple...really simple...js

---
+++ simple.js
@@ -0,0 +1,109 @@
+//integers
+
+let num1 = 1;
+
+let num2 = 2;
+
+let num3 = 3;
+
+let num4 = 4;
+
+let num5 = 5;
+
+let num6 = 6;
+
+let num7 = 7;
+
+let num8 = 8;
+
+let num9 = 9;
+
+let num10 = 10;
+
+//strings
+
+let a = "a";
+
+let b = "b";
+
+let c = "c";
+
+let d = "d";
+
+let e = "e";
+
+let f = "f";
+
+let g = "g";
+
+let h = "h";
+
+let i = "i";
+
+let j = "j";
+
+//arrays
+
+let arr1 = [];
+
+let arr2 = [null];
+
+let arr3 = [undefined, null];
+
+let arr4 = ["1, 2", 3];
+
+let arr5 = [4, 5, "6"];
+
+let arr6 = ["seven", "eight", 9];
+
+let arr7 = [10, [11, 12]];
+
+let arr8 = ["thirteen", [14, [15]]];
+
+let arr9 = [{ sixteen: 16 }, 17, "eighteen"];
+
+let arr10 = ["nineteen", { twenty: 20 }, [21]];
+
+//objects
+
+let obj1 = {
+  one: 1,
+};
+
+let obj2 = {
+  two: 2,
+};
+
+let obj3 = {
+  three: [3],
+};
+
+let obj4 = {
+  four: ["four"],
+};
+
+let obj5 = {
+  five: [{ five: 5 }],
+};
+
+let obj6 = {
+  six() {
+    console.log("six");
+  },
+};
+
+let obj7 = {
+  seven: 7,
+};
+
+let obj8 = {
+  eight: "el gran ocho",
+};
+
+let obj9 = {
+  nine: ["nein, nein, nein"],
+};
+
+let obj10 = {
+  ten: "finally done.... ten",
+};
